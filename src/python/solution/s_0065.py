ecdfc4aea19af1c4574380d5d1aeb903a8eff9c8
nocon <maurice.nocon@adesso.de>
2020-03-09 09:39:28 +0100

    Solved problem 65

--- src/python/solution/s_0065.py
+++ src/python/solution/s_0065.py
@@ -3,3 +3,4 @@ def solve():
     for k in reversed([2, 1, 2] + [1 if x % 3 else 2 + (2 * x // 3) for x in range(1, 97)]):
         num = [num[1], k * num[1] + num[0]]
     return sum(map(int, str(num[1])))
+
