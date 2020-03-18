6409d20d531b5a46e7e2aba96d5f71f555129137
Maurice Nocon <55910883+maucon@users.noreply.github.com>
2020-03-18 13:20:18 +0100

    added c# project

---
+++ src/main/csharp/Starter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectEuler.main.csharp
+{
+    public class Starter
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("ga");
+        }
+    }
+}
