04e1fd2ea7cac5fc1c6d88270fd8ffd4e0efb53d
Maurice Nocon <55910883+maucon@users.noreply.github.com>
2020-03-23 12:40:05 +0100

    faster 500

--- src/main/csharp/p05/Problem0500.cs
+++ src/main/csharp/p05/Problem0500.cs
@@ -1,57 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Numerics;
 
 namespace ProjectEuler.main.csharp.p05
 {
     public class Problem0500
     {
-        public static int Solve()
+        private const int Limit = 7376508; // 7376507 is 500500st prime
+
+        public static ulong Solve()
         {
-            const int limit = 8300000;
-            var primes = Primes(limit);
-            BigInteger solution = 1;
+            var allPrimeSqrt = AllPrimeSqrt(Primes(Limit));
+            allPrimeSqrt.Sort();
 
-            for (var i = 0; i < primes.Count; i++)
+            ulong solution = 1;
+            for (var i = 0; i < 500500; i++)
             {
-                solution *= primes[0];
+                solution *= Convert.ToUInt64(allPrimeSqrt[i]);
                 solution %= 500500507;
-
-                var num = primes[0] * primes[0];
-                primes.Remove(primes[0]);
-
-                var j = 0;
-                while (j < primes.Count && num > primes[j])
-                {
-                    j++;
-                }
-
-                primes.Insert(j, num);
             }
 
-            return (int) solution;
+            return solution;
         }
 
-        private static List<ulong> Primes(int upper)
+        private static List<int> AllPrimeSqrt(List<int> primes)
+        {
+            var pLimit = Limit;
+            for (var i = 2; i < 17; i *= 2)
+            {
+                pLimit = (int) Math.Sqrt(pLimit);
+                for (var j = 0; primes[j] < pLimit + 1; j++) primes.Add(IntPow(primes[j], i));
+            }
+
+            return primes;
+        }
+
+        private static List<int> Primes(int upper)
         {
             var primeSieve = new bool[upper];
-            var primeList = new List<ulong> {2};
+            var primeList = new List<int> {2};
 
             var sqrt = (int) Math.Sqrt(primeSieve.Length);
             for (var i = 3; i < sqrt; i += 2)
                 if (!primeSieve[i])
                 {
-                    primeList.Add(Convert.ToUInt64(i));
+                    primeList.Add(i);
 
                     for (var j = i * i; j < primeSieve.Length; j += i * 2) primeSieve[j] = true;
                 }
 
             for (var i = ((sqrt + 1) & 1) == 0 ? sqrt + 2 : sqrt + 1; i < upper && primeList.Count < 500500; i += 2)
-            {
-                if (!primeSieve[i]) primeList.Add(Convert.ToUInt64(i));
-            }
+                if (!primeSieve[i])
+                    primeList.Add(i);
 
             return primeList;
+        }
+
+        private static int IntPow(int x, int pow)
+        {
+            var ret = 1;
+            while (pow != 0)
+            {
+                if ((pow & 1) == 1)
+                    ret *= x;
+                x *= x;
+                pow >>= 1;
+            }
+
+            return ret;
         }
     }
 }
