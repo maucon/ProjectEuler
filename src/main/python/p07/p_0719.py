4f9ef8a32c72f9bdf22d072735f1cb164a73d82c
maucon <maucon13@gmail.com>
2020-07-24 13:24:14 +0200

    optimized 719

--- src/main/python/p07/p_0719.py
+++ src/main/python/p07/p_0719.py
@@ -1,18 +1,14 @@
 def solve():
-    s = 41333
-    for i in range(10 ** 2 + 1, 10 ** 6 + 1):
-        i_square = i * i
-        string = str(i_square)
-        length = len(string) + 1
-        for j in range(1, 2 ** (length - 2)):
-            bin_str = bin(j)[2:][::-1].ljust(length, "0")
-            split_arr = [x + 1 for x in range(len(bin_str)) if bin_str[x] == "1"]
-            if i == int(string[split_arr[-1]:]) + sum(
-                    [int(string[0 if a == 0 else split_arr[a - 1]:split_arr[a]]) for a in range(len(split_arr))]):
-                s += i_square
-                break
-    return s
+    def can_make(total, digs):
+        if digs == total:
+            return True
+        if digs > total:
+            t = 10
+            while t < digs:
+                rest = digs % t
+                if rest < total and can_make(total - rest, digs // t):
+                    return True
+                t *= 10
+        return False
 
-
-if __name__ == '__main__':
-    print(solve())
+    return 41332 + sum([i * i for i in range(10 ** 2 + 1, 10 ** 6 + 1) if i % 9 < 2 and can_make(i, i * i)])
